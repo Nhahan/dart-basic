469177b88a5d2a5aa118f722f755f3536e9b93fa
Nhahan <kisy324@naver.com>
2023-10-05 07:33:16 +0900

    named parameters

--- Functions/named-parameters.dart
+++ Functions/named-parameters.dart
@@ -1,7 +1,19 @@
-String sayHello(String name, int age, String country) {
+String sayHello({
+  required String name, 
+  required int age, 
+  required String country
+}) {
   return "Hello $name, you are $age, and you come from $country";
 }
 
+// void main() {
+//   print(sayHello('John', 19, 'Korea'));
+// }
+
 void main() {
-  print(sayHello('John', 19, 'Korea'));
+  print(sayHello(
+    name: 'John', 
+    age: 19, 
+    country: 'Korea'
+  ));
 }
