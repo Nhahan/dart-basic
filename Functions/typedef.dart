e24ab023bf010f4e88d6e87ffae5e7e2a3185a6f
Nhahan <kisy324@naver.com>
2023-10-08 11:26:19 +0900

    typedef

---
+++ Functions/typedef.dart
@@ -0,0 +1,15 @@
+typedef ListOfInts = List<int>;
+
+// List<int> reverseListOfNumbers(List<int> list) {
+//   var reversed = list.reversed;
+//   return reversed.toList();
+// }
+
+ListOfInts reverseListOfNumbers(ListOfInts list) {
+  var reversed = list.reversed;
+  return reversed.toList();
+}
+
+void main() {
+  print(reverseListOfNumbers([1, 2, 3]));
+}
