49851a4b13e0616a50b362b84474c10a2cea0162
Nhahan <kisy324@naver.com>
2023-10-10 07:28:31 +0900

    method

--- Classes/data-class.dart
+++ Classes/data-class.dart
@@ -1,8 +1,16 @@
 class Player {
   String name = 'name';
   int age = 15;
+
+  void sayHello() {
+    // var name = 'new name';
+    // print("Hi, my name is ${this.name}");
+
+    print("Hi, my name is $name");
+  }
 }
 
 void main() {
   var player = Player();
+  player.sayHello();
 }
