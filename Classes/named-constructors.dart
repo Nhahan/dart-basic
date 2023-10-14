dbd8c56be8a0c13f2e60099b061252e2a2483889
Nhahan <kisy324@naver.com>
2023-10-14 20:41:41 +0900

    named constructors

---
+++ Classes/named-constructors.dart
@@ -0,0 +1,30 @@
+class Player {
+  final String name;
+  int age;
+  String team;
+
+  Player({
+    required this.name, 
+    required this.age,
+    required this.team
+  });
+
+  Player.createBluePlayer({
+    required String name, 
+    required int age
+}) : this.age = age,
+    this.name = name,
+    this.team = 'blue';
+
+  void sayHello() {
+    print("Hi, my name is $name");
+  }
+}
+
+void main() {
+  var player = Player(name: 'hello', age: 22, team: 'red');
+  var bluePlayer = Player.createBluePlayer(
+    name: 'A', 
+    age: 21,
+  );
+}
