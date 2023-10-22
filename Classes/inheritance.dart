160430f4f3b449a1fc1ccfe5fe52f135cb7de48c
Nhahan <kisy324@naver.com>
2023-10-22 14:40:32 +0900

    inheritance

---
+++ Classes/inheritance.dart
@@ -0,0 +1,30 @@
+class Human {
+  final String name;
+  
+  Human({
+    required this.name
+  });
+
+  void sayHello() {
+    print("Hi! My name is $name");
+  }
+}
+
+enum Team { blue, red }
+
+class Player extends Human {
+  final Team team;
+
+  Player({
+    required this.team,
+    required String name
+  }) : super(name: name);
+}
+
+void main() {
+  var player = Player(
+    team: Team.red,
+    name: "kim"
+  );
+  player.sayHello();
+}
